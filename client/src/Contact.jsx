8cebcbad2539d792629da35476d8d32650b694dc
Avins VR <avins2005@gmail.com>
2025-11-01 10:40:58 +0530

    first commit

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -23,7 +23,14 @@ function Contact() {
   try {
     const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-    await axios.post(`${API_URL}/send-message`, formData);
+    await axios.post(
+  `${API_URL}/send-message`,
+  formData,
+  {
+    withCredentials: true // ✅ required for CORS
+  }
+);
+
 
     setStatus("✅ Message Sent Successfully!");
     setFormData({ name: "", email: "", message: "" });
