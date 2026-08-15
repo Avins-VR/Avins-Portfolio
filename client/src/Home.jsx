47e056b39689b32608fd91e18b6465f34e12cf2d
Avins VR <avins2005@gmail.com>
2026-08-15 19:04:38 +0530

    first commit

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -104,7 +104,7 @@ function App() {
                 whileTap={{ scale: 0.95 }}
               >
                 <a
-                  href="https://drive.google.com/file/d/1YSLfeGFUjg6QYHcdoWh9OCqXTuGt8jcA/view"
+                  href="https://drive.google.com/file/d/1ekOmBPdk20dKPEFLtmlYHhbmjxKrnaJs/view"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
